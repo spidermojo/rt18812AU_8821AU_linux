d49db95737540aa47b4f7f0ce924e7b18875f5d0
kdhalbert <kdhalbert13@gmail.com>
2017-07-13 01:27:45 +0000

    Initial commit

---
+++ woodhouse.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext.commands import Bot
+
+my_bot = Bot(command_prefix="Woodhouse ")
+
+@my_bot.event
+async def on_ready():
+	print ("Client logged in")
+
+@my_bot.command()
+async def echo(*, message: str):
+	"""Echos text following the command"""
+	await my_bot.say(message)
+
+@my_bot.command(pass_context=True)
+async def hello(ctx, member: discord.Member = None):
+	"""Sat hello to Woodhouse!"""
+	if member is None:
+		member = ctx.message.author
+
+	await my_bot.say('Hello {0.name}. How are you today?'.format(member))
+
+@my_bot.command(pass_context=True)
+async def joined(ctx, member: discord.Member = None):
+	"""Says when a member joined"""
+	if member is None:
+		member = ctx.message.author
+
+	await my_bot.say('Hello, {0.name}. Can I make you some coffee?'.format(member))
+
+@my_bot.command()
+async def on_command_error(error, ctx):
+	await my_bot.say('Oops. The command seemed to have broken me. Please refraing from doing it again')
+
+my_bot.run("MzMzNzEzNjIyNjQxNjcyMTky.DEQqyA.iiPTJAgNkZAT1cJtqGIvdHlHIQ0")
